b4c35d8fcfeea1f121970308acf607fe68f45933
sajeshmattathil <135832263+sajeshmattathil@users.noreply.github.com>
2023-11-18 01:54:10 +0530

    git db changed for test

--- Back end/config/dbconnect.js
+++ Back end/config/dbconnect.js
@@ -1,11 +1,11 @@
 const mongoose=require('mongoose')
 
-const dbConnect=async ()=>{ await mongoose.connect('mongodb://127.0.0.1:27017/hotelbooking').then(()=>{
-                     console.log("db is connected");}).catch((error)=>{console.log('error in db connection');})   }
-
-    module.exports=dbConnect
-
-// const dbConnect=async ()=>{ await mongoose.connect('mongodb+srv://sajeshmattathil:949740halla@cluster0.uhzzvba.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+// const dbConnect=async ()=>{ await mongoose.connect('mongodb://127.0.0.1:27017/hotelbooking').then(()=>{
 //                      console.log("db is connected");}).catch((error)=>{console.log('error in db connection');})   }
 
-//     module.exports=dbConnect  
+//     module.exports=dbConnect
+
+const dbConnect=async ()=>{ await mongoose.connect('mongodb+srv://sajeshmattathil:949740halla@cluster0.uhzzvba.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+                     console.log("db is connected");}).catch((error)=>{console.log('error in db connection');})   }
+
+    module.exports=dbConnect  
